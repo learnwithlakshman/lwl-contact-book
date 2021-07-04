b9b11278d7293d3b60297851d3c7e49a27d2eb11
avulalakshman <learnwithlakshman@gmail.com>
2021-07-04 07:55:17 +0530

    Contact Book

---
+++ src/test/java/com/lwl/lwlcontactbook/LwlContactBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lwl.lwlcontactbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LwlContactBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
